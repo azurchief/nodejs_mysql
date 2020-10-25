1870155df9fd9684624ceb19ed6abaa922519fec
azurchief <67046863+azurchief@users.noreply.github.com>
2020-10-25 11:40:06 +0900

    fixed 2

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var mysql = require('mysql');
 var db = mysql.createConnection({
   host:'localhost',
   user:'nodejs',
-  password:'kid14128',
+  password:'111111',
   database:'opentutorials'
 });
 db.connect();
--- nodejs/mysql.js
+++ nodejs/mysql.js
@@ -2,7 +2,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'nodejs',
-  password : '111111', /* 이건 폼이 아니라 진짜 연결에 쓰이는 것. kid14128 하면 연결안됨. 원인은 모름 */
+  password : '111111', /* 이건 폼이 아니라 진짜 연결에 쓰이는 것. 이 암호 맞음. */
   database : 'opentutorials'
 });
  
